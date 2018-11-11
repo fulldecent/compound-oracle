9c4569365392f293ad6b41754449b542b7b200fa
William Entriken <github.com@phor.net>
2018-11-11 00:03:55 -0500

    Typo

--- contracts/Exponential.sol
+++ contracts/Exponential.sol
@@ -4,7 +4,7 @@ import "./ErrorReporter.sol";
 import "./CarefulMath.sol";
 
 /**
-  * @title Exponential module for storing fixed-decision decimals
+  * @title Exponential module for storing fixed-point decimals
   * @author Compound
   * @notice Exp is a struct which stores decimals with a fixed precision of 18 decimal places.
   *         Thus, if we wanted to store the 5.1, mantissa would store 5.1e18. That is:
